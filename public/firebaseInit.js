dd8b808d32d694e9b6cca66bc19125e63229fdf3
Adonis Mendoza <adonis.mendoza@outlook.com>
2018-07-16 10:32:55 -0400

    [Add] firebaseInit - instancia de firebase

---
+++ public/firebaseInit.js
@@ -0,0 +1,12 @@
+(function (){
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyAphHJJBP2HpPNynnQgZ66GkAr23wtLEiA",
+        authDomain: "ciudada-no.firebaseapp.com",
+        databaseURL: "https://ciudada-no.firebaseio.com",
+        projectId: "ciudada-no",
+        storageBucket: "ciudada-no.appspot.com",
+        messagingSenderId: "896456539633"
+    };
+    firebase.initializeApp(config);
+})()
