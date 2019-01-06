225c363452b7da04d282ca1013f058f860535b4f
adonismendozaperez <adonis.mendoza@outlook.com>
2019-01-05 20:14:34 -0400

    [Change] Login Page, Optimize and perfomance this page

--- public/Login.js
+++ public/Login.js
@@ -1,118 +1,7 @@
 (function (){
-    //CREAR NUEVO USUARIO
-    $(".btnCrear").click(function(e){
-        e.preventDefault();
-        NuevoUsuario($("#RegistroInputEmail").val(), $("#RegistroInputPassword").val());
-    });
-
-    //INICIAR SESION
-    $(".btnIniciarSesion").click(function(e){
-        e.preventDefault();
-        Login($("#LoginInputEmail").val(), $("#LoginInputPassword").val());
-    });
-
     $(".btnIniciarSesionGoogle").click(function(e){
-        e.preventDefault();
         GoogleAuth();
     })
-    
-    function Login(user,pass){
-        firebase.auth().signInWithEmailAndPassword(user, pass)
-        .then(()=>{
-            $("#LoginInputEmail").val("");
-            $("#LoginInputPassword").val("");
-            localStorage.setItem("UserName",user)
-            
-            if(user === "adonis.mendoza@outlook.com"){
-                location.href = 'Admin.html';
-            }
-            else{
-                location.href = "Dashboard.html";
-            }
-
-        })
-        .catch(function(error) {
-            let errorCode = error.code;
-            let errorMessage = error.message;
-            if (errorCode === 'auth/wrong-password') {
-                swal({
-                    title: "Información!",
-                    text: "Contraseña incorrecta",
-                    icon: "warning",
-                    button: "Okey!",
-                  });
-            }
-            else if(errorCode === 'auth/invalid-email'){
-                swal({
-                    title: "Información!",
-                    text: "La dirección de correo electrónico está mal, favor de veríficar.",
-                    icon: "warning",
-                    button: "Okey!",
-                  });
-            }
-            else {
-                swal({
-                    title: "Información!",
-                    text: errorMessage,
-                    icon: "warning",
-                    button: "Okey!",
-                  });
-            }
-        });
-    }
-
-    function NuevoUsuario(user,pass){
-        firebase.auth().createUserWithEmailAndPassword(user, pass)
-        .then(function(){
-            Login(user,pass);
-            $("#RegistroInputEmail").val("");
-            $("#RegistroInputPassword").val("");
-        })
-        .catch(function(error) {
-            let errorCode = error.code;
-            let errorMessage = error.message;
-            if (errorCode == 'auth/weak-password') {
-                swal({
-                    title: "Información!",
-                    text: "La contraseña es muy débil!",
-                    icon: "warning",
-                    button: "Okey!",
-                    });
-                }
-                else if(errorCode == 'auth/email-already-in-use'){
-                swal({
-                    title: "Información!",
-                    text: "Esta cuenta ya está en uso, favor de intentar con otra!",
-                    icon: "warning",
-                    button: "Okey!",
-                    });
-                }
-                else if(errorCode == 'auth/invalid-email'){
-                swal({
-                    title: "Información!",
-                    text: "La dirección de correo electrónico no es válida!",
-                    icon: "warning",
-                    button: "Okey!",
-                    });
-                }
-                else if(errorCode == 'auth/weak-password'){
-                swal({
-                    title: "Información!",
-                    text: "La contraseña no es lo suficientemente fuerte, favor de intentar con otra!",
-                    icon: "warning",
-                    button: "Okey!",
-                    });
-                }
-                else{
-                swal({
-                    title: "Información!",
-                    text: errorMessage,
-                    icon: "warning",
-                    button: "Okey!",
-                    });
-                }
-        });
-    }
 
     function GoogleAuth (){
         var provider = new firebase.auth.GoogleAuthProvider();
@@ -120,7 +9,6 @@
             localStorage.setItem("UserName",result.user.email)
             location.href = "Dashboard.html";
           }).catch(function(error) {
-            var errorCode = error.code;
             var errorMessage = error.message;
             var email = error.email;
             var credential = error.credential;
@@ -132,5 +20,4 @@
             });
         });
     }
-
 })();
